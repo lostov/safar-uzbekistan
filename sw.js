36ca001547719bedbd678934b33288371a6b558e
lostov <manuchehr@jalolov.ru>
2026-05-04 11:31:36 +0500

    Added SW self-update system

--- sw.js
+++ sw.js
@@ -61,7 +61,7 @@ self.addEventListener('install', (event) => {
     self.skipWaiting(); 
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
+            return cache.addAll(cacheUrls);
         })
     );
 });
